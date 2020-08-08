c11d699293eebe94eabbb04e2f7733a2a563f8b2
sagar-uprety <sagaruprety222@gmail.com>
2020-08-08 13:35:56 +0545

    SignUp with Image picker

--- lib/screens/signup/signup_main.dart
+++ lib/screens/signup/signup_main.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
 import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+
 // import 'package:sahayogihaath/screens/dashboard.dart';
 import '../dashboard.dart';
 import './signup.dart';
@@ -20,19 +26,45 @@ class _SignUpMainState extends State<SignUpMain> {
   void _submitSignUpForm(
     String email,
     String password,
+    String name,
+    String phone,
+    String address,
+    File userImage,
     BuildContext ctx,
   ) async {
     AuthResult authResult;
+
     try {
       setState(() {
         _isLoading = true;
       });
+
       authResult = await _auth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
+
+      final ref = FirebaseStorage.instance
+          .ref()
+          .child('user_image')
+          .child(authResult.user.uid + '.jpg');
+
+      await ref.putFile(userImage).onComplete;
+      final url = await ref.getDownloadURL();
+
+      await Firestore.instance
+          .collection('users')
+          .document(authResult.user.uid)
+          .setData({
+        'name': name,
+        'email': email,
+        'phone': phone,
+        'address': address,
+        'image_url': url,
+      });
       //TODO: This should be auto handled by authStateChanged
       Navigator.pushReplacementNamed(context, Dashboard.id);
+      
     } on PlatformException catch (err) {
       var message = 'An error occurred, pelase check your credentials!';
       if (err.message != null) {
