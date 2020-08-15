71b34a0283fcef26d33e2597cb6f8dc6e6aeaddc
sagar-uprety <sagaruprety222@gmail.com>
2020-08-15 19:14:36 +0545

    khalti test

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,9 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_khalti/flutter_khalti.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   static const id = 'dashboard';
+
   @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  _payViaKhalti() async {
+    FlutterKhalti _flutterKhalti = FlutterKhalti.configure(
+      publicKey: "test_public_key_eacadfb91994475d8bebfa577b0bca68",
+      urlSchemeIOS: "KhaltiPayFlutterExampleScheme",
+      paymentPreferences: [
+        KhaltiPaymentPreference.KHALTI,
+      ],
+    );
+
+    KhaltiProduct product = KhaltiProduct(
+      id: "test",
+      amount: 1000,
+      name: "Hello Product",
+    );
+    _flutterKhalti.startPayment(
+      product: product,
+      onSuccess: (data) {
+        print("here");
+      },
+      onFaliure: (error) {
+        print("sorry");
+      },
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
@@ -15,6 +47,10 @@ class Dashboard extends StatelessWidget {
                 'Dashboard',
               ),
             ),
+          ),
+          MaterialButton(
+            child: Text('Pay via Khalti'),
+            onPressed: _payViaKhalti,
           ),
           RaisedButton(
             onPressed: () {
