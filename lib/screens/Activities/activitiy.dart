382b646ab6e8e9f43583d694aa3e4b91c65eb6c8
sagar-uprety <sagaruprety222@gmail.com>
2020-08-20 23:41:47 +0545

    info 1 q .

--- lib/screens/Activities/activitiy.dart
+++ lib/screens/Activities/activitiy.dart
@@ -1,49 +1,49 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../../provider_/activity_provider.dart';
-import 'product.dart';
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import '../../provider_/activity_provider.dart';
+// import 'product.dart';
 
-class ActivitiesScreen extends StatefulWidget {
-  static const id = "activities_form";
-  @override
-  _ActivitiesScreenState createState() => _ActivitiesScreenState();
-}
-
-class _ActivitiesScreenState extends State<ActivitiesScreen> {
-  @override
-  Widget build(BuildContext context) {
-    final productProvider = Provider.of<ActivityProvider>(context);
-    return Scaffold(
-      body: ListView(
-        children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-              hintText: 'title',
-            ),
-            onChanged: (value) {
-              productProvider.changeName(value);
-            },
-          ),
-          TextField(
-            decoration: InputDecoration(
-              hintText: 'description',
-            ),
-            onChanged: (value) {
-              productProvider.changeDescription(value);
-            },
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          RaisedButton(
-            child: Text('Submit'),
-            onPressed: () {
-              productProvider.saveActivity();
-              Navigator.pushNamed(context, Products.id);
-            },
-          ),
-        ],
-      ),
-    );
-  }
-}
+// class ActivitiesScreen extends StatefulWidget {
+//   static const id = "activities_form";
+//   @override
+//   _ActivitiesScreenState createState() => _ActivitiesScreenState();
+// }
+// //
+// class _ActivitiesScreenState extends State<ActivitiesScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final productProvider = Provider.of<ActivityProvider>(context);
+//     return Scaffold(
+//       body: ListView(
+//         children: <Widget>[
+//           TextField(
+//             decoration: InputDecoration(
+//               hintText: 'title',
+//             ),
+//             onChanged: (value) {
+//               productProvider.changeName(value);
+//             },
+//           ),
+//           TextField(
+//             decoration: InputDecoration(
+//               hintText: 'description',
+//             ),
+//             onChanged: (value) {
+//               productProvider.changeDescription(value);
+//             },
+//           ),
+//           SizedBox(
+//             height: 20.0,
+//           ),
+//           RaisedButton(
+//             child: Text('Submit'),
+//             onPressed: () {
+//               productProvider.saveActivity();
+//               Navigator.pushNamed(context, Products.id);
+//             },
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
--- lib/screens/activities_screen/activity_info.dart
+++ lib/screens/activities_screen/activity_info.dart
@@ -1,14 +1,131 @@
 import 'package:flutter/material.dart';
+import '../../model_demo/dactor_model.dart';
+import '../../theme/extention.dart';
+import '../../theme/light_color.dart';
+import '../../theme/text_styles.dart';
+import '../../theme/theme.dart';
+
+import '../../components/RoundedButton.dart';
 
 class ActivityInfo extends StatefulWidget {
   static const id = "activity_info";
+
+  ActivityInfo({Key key, this.model}) : super(key: key);
+  final DoctorModel model;
   @override
   _ActivityInfoState createState() => _ActivityInfoState();
 }
 
 class _ActivityInfoState extends State<ActivityInfo> {
+  DoctorModel model;
+  @override
+  void initState() {
+    model = widget.model;
+    super.initState();
+  }
+
+  Widget _appbar() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        BackButton(color: Theme.of(context).primaryColor),
+        IconButton(
+            icon: Icon(
+              model.isfavourite ? Icons.favorite : Icons.favorite_border,
+              color: model.isfavourite ? Colors.red : LightColor.grey,
+            ),
+            onPressed: () {
+              setState(() {
+                model.isfavourite = !model.isfavourite;
+              });
+            })
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    TextStyle titleStyle = TextStyles.title.copyWith(fontSize: 25).bold;
+    if (AppTheme.fullWidth(context) < 393) {
+      titleStyle = TextStyles.title.copyWith(fontSize: 23).bold;
+    }
+    return Scaffold(
+      backgroundColor: LightColor.extraLightBlue,
+      body: SafeArea(
+        bottom: false,
+        child: Stack(
+          children: <Widget>[
+            Image.asset('assets/images1/ben.jpg'),
+            DraggableScrollableSheet(
+              maxChildSize: .8,
+              initialChildSize: .6,
+              minChildSize: .6,
+              builder: (context, scrollController) {
+                return Container(
+                  height: AppTheme.fullHeight(context) * .5,
+                  padding: EdgeInsets.only(
+                      left: 19,
+                      right: 19,
+                      top: 16), //symmetric(horizontal: 19, vertical: 16),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30)),
+                    color: Colors.white,
+                  ),
+                  child: SingleChildScrollView(
+                    physics: BouncingScrollPhysics(),
+                    controller: scrollController,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        ListTile(
+                          contentPadding: EdgeInsets.all(0),
+                          title: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                model.name,
+                                style: titleStyle,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              // Icon(Icons.check_circle,
+                              //     size: 18,
+                              //     color: Theme.of(context).primaryColor),
+                              // Spacer(),
+                              // RatingStar(
+                              //   rating: model.rating,
+                              // )
+                            ],
+                          ),
+                          subtitle: Text(
+                            model.type,
+                            style: TextStyles.bodySm.subTitleColor.bold,
+                          ),
+                        ),
+                        Divider(
+                          thickness: .3,
+                          color: LightColor.grey,
+                        ),
+                        Text("About", style: titleStyle).vP16,
+                        Text(
+                          model.description,
+                          textAlign: TextAlign.justify,
+                          style: TextStyles.body.subTitleColor,
+                        ),
+                        RoundButton(text: "Donate")
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+            _appbar(),
+          ],
+        ),
+      ),
+    );
   }
 }
