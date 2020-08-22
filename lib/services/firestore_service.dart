66790a5b6b41b3e2e0d0e3ad53499b881e8c595f
sagar-uprety <sagaruprety222@gmail.com>
2020-08-22 13:13:39 +0545

    merged

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -18,13 +18,8 @@ class FirestoreService {
         .map((document) => Activity.fromFirestore(document.data))
         .toList());
   }
-<<<<<<< HEAD
 
   Future<void> removeActivity(String activityId) {
     return _db.collection('activities').document(activityId).delete();
-=======
-  Future<void> removeActivity(String description){
-    return _db.collection('activities').document(ActivitiesList.id).delete();
->>>>>>> 5d38c0432e9704ac7aed239884a6a461d9894f0f
   }
 }
