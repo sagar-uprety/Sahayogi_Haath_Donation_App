f3ff9e7c7d9eb8317aab6c70b630c19627f14f2b
sagar-uprety <sagaruprety222@gmail.com>
2020-08-23 11:30:14 +0545

    latest change

--- lib/screens/donate_screen/donate_main.dart
+++ lib/screens/donate_screen/donate_main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sahayogihaath/components/RoundedButton.dart';
 import 'package:sahayogihaath/provider_/auth_provider.dart';
 import '../../theme/extention.dart';
+
+enum medium { eSewa, khalti }
 
 class DonateScreen extends StatefulWidget {
   static const id = "donate_screen";
@@ -13,22 +16,52 @@ class _DonateScreenState extends State<DonateScreen> {
   int donateAmount;
   DateTime date;
   String username;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    getUserDetails();
-    super.initState();
-  }
+  String selectedmedium;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: _appBar(),
-        body: Container(
-            // child: Text(username),
-            ),
+        body: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {
+                    donateAmount = int.parse(value);
+                  });
+                },
+              ),
+              DropdownButtonFormField(
+                  items: [
+                    DropdownMenuItem(
+                      value: medium.eSewa,
+                      child: Text("eSewa"),
+                    ),
+                    DropdownMenuItem(
+                      value: medium.khalti,
+                      child: Text("Khalti"),
+                    )
+                  ],
+                  onChanged: (value) {
+                    selectedmedium = value;
+                    print(value);
+                  }),
+              RoundButton(
+                text: "Donate Rs.${donateAmount.toString()} ",
+                onPress: () {
+                  _formKey.currentState.save();
+                  print(selectedmedium.toString());
+                  print(donateAmount);
+                },
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
