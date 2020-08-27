987fee2fc29d51b128a1c8262b4b4220c67c3f24
sagar-uprety <sagaruprety222@gmail.com>
2020-08-27 21:25:04 +0545

    esewa v2.0

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,67 +1,120 @@
-import 'package:flutter/material.dart';
-
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:esewa_pnp/esewa.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:esewa_pnp/esewa_pnp.dart';
 
-import '../components/RoundedButton.dart';
+class Dashboard extends StatefulWidget {
+  static const id = "Dashboard";
+  @override
+  _DashboardState createState() => _DashboardState();
+}
 
-class Dashboard extends StatelessWidget {
-  static const id = 'dashboard';
+class _DashboardState extends State<Dashboard> {
+  ESewaPnp _esewaPnp;
+  ESewaConfiguration _configuration;
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _configuration = ESewaConfiguration(
+      clientID: "NhINFhgcDAxZLRIEAwlTLwoXBAcMGA9JJTVUICBIJCA7Kjw2Ijo=",
+      secretKey: "NhINFhgcDAxZLRIEAwk=",
+      environment: ESewaConfiguration.ENVIRONMENT_TEST,
+    );
+    _esewaPnp = ESewaPnp(configuration: _configuration);
+  }
+
+  int _amount = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            child: Center(
-              child: Text(
-                'Dashboard',
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color.fromRGBO(65, 161, 36, 1),
+      ),
+      home: Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: Text("ESewa PNP"),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {
+                    _amount = int.parse(value);
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: "Enter amount",
+                ),
               ),
-            ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                width: double.infinity,
+                child: RaisedButton(
+                  color: Color.fromRGBO(65, 161, 36, 1),
+                  onPressed: () {
+                    pay();
+                  },
+                  child: Text(
+                    "Pay",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 84,
+              ),
+              RaisedButton(
+                onPressed: () async {
+                  await FirebaseAuth.instance.signOut();
+                },
+                child: Text('Logout'),
+              )
+            ],
           ),
-          RoundButton(
-              text: 'Donate Rs 10',
-              onPress: () async {
-                await initEpayment();
-              }),
-          RaisedButton(
-            onPressed: () async {
-              await FirebaseAuth.instance.signOut();
-            },
-            child: Text('Logout'),
-          )
-        ],
+        ),
       ),
     );
   }
-}
 
-initEpayment() async {
-  ESewaConfiguration _configuration = ESewaConfiguration(
-    clientID: "NhINFhgcDAxZLRIEAwlTLwoXBAcMGA9JJTVUICBIJCA7Kjw2Ijo=",
-    secretKey: "NhINFhgcDAxZLRIEAwk=",
-    environment: ESewaConfiguration.ENVIRONMENT_TEST,
-  );
+  Future<void> pay() async {
+    ESewaPayment eSewaPayment = ESewaPayment(
+      amount: _amount,
+      productName: "Org Test 1",
+      productID: "abc123001",
+      callBackURL: "https://www.sagaruprety.com.np",
+    );
+    try {
+      final res = await _esewaPnp.initPayment(payment: eSewaPayment);
+      print(res.productId);
+      print(res.referenceId);
+      print(res.date);
+      _scaffoldKey.currentState.showSnackBar(
+          _buildSnackBar(Color.fromRGBO(65, 161, 36, 1), res.message));
+    } on ESewaPaymentException catch (e) {
+      _scaffoldKey.currentState
+          .showSnackBar(_buildSnackBar(Colors.red, e.message));
+    }
+  }
 
-  ESewaPnp _eSewaPnp = ESewaPnp(configuration: _configuration);
-
-  ESewaPayment _payment = ESewaPayment(
-    amount: 500,
-    productName: 'Demo_org',
-    productID: '001',
-    callBackURL: 'https://sahayaogi_haath.com',
-  );
-
-  final _res = await _eSewaPnp.initPayment(payment: _payment);
-
-  _res.fold(
-    (failure) {
-      print(failure.message);
-    },
-    (success) {
-      print(success.message);
-    },
-  );
+  Widget _buildSnackBar(Color color, String msg) {
+    return SnackBar(
+      backgroundColor: color,
+      content: Text(msg),
+    );
+  }
 }
