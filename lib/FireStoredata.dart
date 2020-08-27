e155157f23f4aa1668acd542b724cca1dc8eacb3
blankmindshreya <blankmindshreya@gmail.com>
2020-08-27 13:09:25 +0545

    Firestore data

---
+++ lib/FireStoredata.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+String user = 'donor';
+String organization = 'donee';
+String userImage = 'donorImage';
+String amount = 'amount';
+String date = 'dateTime';
