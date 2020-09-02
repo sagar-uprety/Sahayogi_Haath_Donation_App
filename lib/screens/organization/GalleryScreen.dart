dd81678053d53241ab2735839432c641699f02a6
nistasimkhada977 <58097961+nistasimkhada977@users.noreply.github.com>
2020-09-02 09:22:36 +0545

    galleryscreen

--- lib/screens/organization/GalleryScreen.dart
+++ lib/screens/organization/GalleryScreen.dart
@@ -4,6 +4,7 @@ class GalleryScreen extends StatefulWidget {
   @override
   _GalleryScreenState createState() => _GalleryScreenState();
 }
+
 class _GalleryScreenState extends State<GalleryScreen> {
   @override
   Widget build(BuildContext context) {
@@ -12,88 +13,87 @@ class _GalleryScreenState extends State<GalleryScreen> {
       body: getBody(),
     );
   }
-   Widget getBody(){
-    return SafeArea(child: SingleChildScrollView(child: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30,vertical: 30),
-      child: Column(
-        children: <Widget>[
-           SizedBox(height: 40,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+  Widget getBody() {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+          child: Column(
             children: <Widget>[
-            Text("Photos",style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold
-            ),),
-            // SvgPicture.asset("assets/images/forward_icon.svg")
-          ],),
-          SizedBox(height: 30,),
-          Column(
-            children: <Widget>[
-                Row(
+              SizedBox(
+                height: 40,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Container(
-                  width: (MediaQuery.of(context).size.width - 80) / 2,
-                  height: 230,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: DecorationImage(image: AssetImage("assets/images1/children.jpg"),fit: BoxFit.cover)
+                  Text(
+                    "Photos",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                   ),
-                ),
-                 SizedBox(width: 20,),
-                  Container(
-                  width: (MediaQuery.of(context).size.width - 80) / 2,
-                  height: 230,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: DecorationImage(image: AssetImage("assets/images1/children.jpg"),fit: BoxFit.cover)
-                  ),
-                ),
-                ],
-                ),
-                SizedBox(height: 20,),
-                  Row(
-                children: <Widget>[
-                Container(
-                width: (MediaQuery.of(context).size.width - 80) / 2,
-                  height: 230,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: DecorationImage(image: AssetImage("assets/images1/janko.jpg"),fit: BoxFit.cover)
-                  ),
-                ),
+                  // SvgPicture.asset("assets/images/forward_icon.svg")
                 ],
               ),
-              SizedBox(height: 20,),
+              SizedBox(
+                height: 30,
+              ),
               Row(
                 children: <Widget>[
                   Container(
-                  width: (MediaQuery.of(context).size.width - 80) / 2,
-                  height: 230,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: DecorationImage(image: AssetImage("assets/images1/jelleke.jpg"),fit: BoxFit.cover)
+                    width: (MediaQuery.of(context).size.width - 80) / 2,
+                    height: 230,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                            image: AssetImage("assets/images1/children.jpg"),
+                            fit: BoxFit.cover)),
                   ),
-                ),
-                SizedBox(height: 20,),
-                Container(
-                                   width: (MediaQuery.of(context).size.width - 80) / 2,
-                  height: 230,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: DecorationImage(image: AssetImage("assets/images1/pham.jpg"),fit: BoxFit.cover)
+                  SizedBox(
+                    width: 20,
                   ),
-                ),
+                  Container(
+                    width: (MediaQuery.of(context).size.width - 80) / 2,
+                    height: 230,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                            image: AssetImage("assets/images1/children.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
                 ],
               ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: <Widget>[
+                  Container(
+                    width: (MediaQuery.of(context).size.width - 80) / 2,
+                    height: 230,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                            image: AssetImage("assets/images1/jelleke.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: (MediaQuery.of(context).size.width - 80) / 2,
+                    height: 230,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                            image: AssetImage("assets/images1/pham.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                ],
+              )
             ],
-          )
-        ],
+          ),
+        ),
       ),
-       
-      ),
-    ),
     );
-   }
+  }
 }
-   
